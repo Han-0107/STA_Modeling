9db0d4576f1b84688e27bc8261cce42e2328291c
Han-0107 <1312692068@qq.com>
2024-08-03 11:31:16 +0800

    optimized

---
+++ README.md
@@ -0,0 +1,11 @@
+# STA Modeling by hspice
+
+1. Put the basic circuit to "./Circuits/XXX.cir".
+1. run "python ./tools/generate_hspice.py".
+2. Copy run_hspice.py to an empty folder (like ./Circuits/simulated/).
+3. run "python run_hspice.py".
+4. run "find /path/to/simulated -type f ! -name "*.mt0" -delete".
+5. run "python post_process.py".
+6. The .csv files are what you need.
+
+* Review the output files!
---
+++ gates/Circuits/results/post_process1.py
@@ -0,0 +1,52 @@
+import os
+import re
+import csv
+import glob
+
+def extract_data_to_csv(file_path, csv_writer):
+    filename = os.path.basename(file_path)
+    second_value = None
+    third_value = None
+    fourth_value = None
+    if 'VOL_' in filename:
+        match = re.search(r'VOL_(\d+\.\d+)', filename)
+        if match:
+            second_value = match.group(1)
+    if 'Trans_' in filename:
+        match = re.search(r'Trans_(\d+\.\d+)', filename)
+        if match:
+            third_value = match.group(1)
+    if 'Cap_' in filename:
+        match = re.search(r'Cap_(\d+\.\d+)', filename)
+        if match:
+            fourth_value = match.group(1)
+    
+    with open(file_path, "r") as file:
+        data = file.read()
+    pattern = r"\d+\.\d+e-\d+"
+    matches = re.findall(pattern, data)
+    if len(matches) >= 2:
+        second_match = matches[1]
+        csv_writer.writerow([second_match, second_value, third_value, fourth_value])
+    else:
+        print(f"Less than two matches found in {file_path}")
+        csv_writer.writerow(['', second_value, third_value, fourth_value])
+
+def process_files(folder_path):
+    files = glob.glob(os.path.join(folder_path, "*.mt0"))
+    if not files:
+        print(f"No .mt0 files found in {folder_path}")
+        return
+    
+    csv_file = "All_data.csv"
+    with open(csv_file, mode='w', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(["Data", "VOL", "Trans", "Cap"])
+        for file_path in files:
+            extract_data_to_csv(file_path, csv_writer)
+    
+    print(f"Data extracted from {len(files)} files saved to {csv_file}")
+
+if __name__ == "__main__":
+    folder_path = r"/data/yaohuihan/Research/STA_Modeling/gates/Circuits/simulated"
+    process_files(folder_path)
