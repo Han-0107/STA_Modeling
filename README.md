d83fcce1278b340f421848cf1c417082a3c4b0ad
Han-0107 <1312692068@qq.com>
2024-08-11 18:54:26 +0800

    tools

--- README.md
+++ README.md
@@ -16,17 +16,4 @@
 2. subckt: /data/yaohuihan/Research/SAED32nm/SAED32nm_EDK/lib/stdcell_rvt/lvs/saed32nm_rvt.cdl
 3. NLDM libs: /data/yaohuihan/Research/SAED32nm/SAED32nm_EDK/lib/stdcell_rvt
 
-## Design Compiler如何自定义库文件进行综合和时序分析？
 
-* 首先，读入自定义库：set target_library "gscl45nm.db", set link_library "* gscl45nm.db"
-* 读入verilog设计：read_verilog adder.v
-* 建立关联：link
-* 编译：compile
-* 读入sdc文件：read_sdc adder.sdc
-* 查看使用的cell：report_cell
-* 查看时序报告：report_timing
-
-## Library Compiler将.lib文件转换为.db文件
-
-lc_shell> read_lib xx.lib
-lc_shell > write_lib -format db -output xx.db xx
---
+++ tools/post_process2.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+
+file_path = '/data/yaohuihan/Research/STA_Modeling/gates/Circuits/results/All_data.csv'
+df = pd.read_csv(file_path)
+vol_values = df['VOL'].unique()
+
+# 定义文件夹路径以分别存储CSV文件和pivot CSV文件
+normal_folder_path = '/data/yaohuihan/Research/STA_Modeling/gates/Circuits/results'
+pivot_folder_path = '/data/yaohuihan/Research/STA_Modeling/gates/Circuits/results/pivot'
+
+# 创建文件夹，如果不存在
+os.makedirs(normal_folder_path, exist_ok=True)
+os.makedirs(pivot_folder_path, exist_ok=True)
+
+# 创建并保存子集为单独的 CSV 文件
+for vol_value in vol_values:
+    subset_df = df[df['VOL'] == vol_value].copy()
+    output_file = f'vol_{vol_value}_pivot.csv'
+    # 使用 .loc 来安全地修改数据
+    subset_df.loc[:, 'Tplh'] = subset_df['Tplh'].apply(lambda x: float(x) * (10 ** 12))
+    subset_df.to_csv(os.path.join(pivot_folder_path, output_file), index=False, columns=['Trans', 'Cap', 'Tplh'])
+
+for filename in os.listdir(pivot_folder_path):
+    if filename.startswith('vol_') and filename.endswith('_pivot.csv'):
+        file_path = os.path.join(pivot_folder_path, filename)
+        df = pd.read_csv(file_path)
+        pivot_df = df.pivot(index='Trans', columns='Cap', values='Tplh')
+        pivot_file = filename.replace('_pivot.csv', '.csv')
+        pivot_df.to_csv(os.path.join(normal_folder_path, pivot_file))
+
+        # 加载 CSV 文件并清空 A1 单元格
+        pivot_df = pd.read_csv(os.path.join(normal_folder_path, pivot_file))
+        pivot_df.to_csv(os.path.join(normal_folder_path, pivot_file), index=False)
